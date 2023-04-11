524f16e48881d70271da51f24ee4eaa4cee9dfc4
hailm <hai.lemanh.1310@gmail.com>
2023-04-12 01:36:50 +0700

    update

--- template-parts/layouts/section4.php
+++ template-parts/layouts/section4.php
@@ -1,1 +1,96 @@
-<div class="section uk-section home__section home__section4" style="--url:url(images/7c764253fe2281eec4dcb3921451482a.jpeg)"></div>
+<div class="section uk-flex uk-flex-middle uk-section home__section home__section4" style="--url:url(images/7c764253fe2281eec4dcb3921451482a.jpeg)">
+    <div class="uk-width">
+        <div class="uk-container">
+            <div class="uk-flex-center" uk-grid>
+                <div class="uk-width-auto">
+                    <div class="section4__box uk-flex uk-flex-middle">
+                        <div class="uk-width">
+                            <div class="uk-child-width-expand" uk-grid>
+                                <div>
+                                    <div class="uk-text-center" style="color: #3E7EC9;">
+                                        <div class="section4__box__txt1">100</div>
+                                        <div class="section4__box__txt2">Player</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="uk-text-center" style="color: #AF9533;">
+                                        <div class="section4__box__txt1">100</div>
+                                        <div class="section4__box__txt2">Win</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="uk-text-center" style="color: #DF7D46;">
+                                        <div class="section4__box__txt1">20</div>
+                                        <div class="section4__box__txt2">Return</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item__48-24" uk-slider>
+
+                <div class="uk-position-relative">
+
+                    <div class="uk-slider-container uk-light">
+                        <ul class="uk-slider-items uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-grid-small uk-grid-36-m" uk-grid>
+                            <?php for ($i=1;$i<=6;$i++): ?>
+                            <li>
+                                <div class="uk-card uk-card-body section4__card">
+                                    <div class="section4__card__quote">“</div>
+                                    <div class="section4__card__txt">Send texts, group messages, pictures, videos, and emoji from your phone or the web.</div>
+                                    <div class="uk-flex-bottom uk-flex-right item__16 uk-grid-16" uk-grid>
+                                        <div class="uk-width-auto">
+                                            <div class="uk-cover-container uk-border-circle uk-flex-inline">
+                                                <img src="images/noimage.jpg" alt="" uk-cover="">
+                                                <canvas width="36" height="36"></canvas>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div class="uk-position-relative uk-text-nowrap">
+                                                <i class="section2__star section2__star--off"></i>
+                                                <i class="section2__star section2__star--off"></i>
+                                                <i class="section2__star section2__star--off"></i>
+                                                <i class="section2__star section2__star--off"></i>
+                                                <i class="section2__star section2__star--off"></i>
+                                                <div class="uk-position-center-left uk-overflow-hidden" style="width: 89%">
+                                                    <i class="section2__star section2__star--on"></i>
+                                                    <i class="section2__star section2__star--on"></i>
+                                                    <i class="section2__star section2__star--on"></i>
+                                                    <i class="section2__star section2__star--on"></i>
+                                                    <i class="section2__star section2__star--on"></i>
+                                                </div>
+                                            </div>
+                                            <div class="item__16 section4__card__name">John Williams</div>
+                                            <div class="section4__card__txtsmall">Lead designer</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <?php endfor; ?>
+                        </ul>
+                    </div>
+
+                    <div class="uk-hidden@s uk-light">
+                        <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                        <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                    </div>
+
+                    <div class="uk-visible@s">
+                        <a class="uk-position-center-left-out uk-position-small uk-link-toggle section2__slidenav section2__slidenav__previous" href="#" uk-slider-item="previous">
+                            <div></div>
+                        </a>
+                        <a class="uk-position-center-right-out uk-position-small uk-link-toggle section2__slidenav section2__slidenav__next" href="#" uk-slider-item="next">
+                            <div></div>
+                        </a>
+                    </div>
+
+                </div>
+
+                <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin uk-hidden@m"></ul>
+
+            </div>
+        </div>
+    </div>
+</div>
