69840b15bfd45211e074dfb2af0fc8fbc4420e2b
hailm <hai.lemanh.1310@gmail.com>
2023-04-19 14:07:53 +0700

    fix

--- nofication.php
+++ nofication.php
@@ -10,6 +10,6 @@
     <?php require "template-parts/layouts/section5.php"; ?>
 </div>
 <script>
-    UIkit.notification({message:'<span class="nofication__iconSuccess" uk-icon="icon: check"></span> The action that you have done was a success! Well done', pos: 'top-right',status:'success',timeout:0});
+    UIkit.notification({message:'<span class="nofication__iconSuccess" uk-icon="icon: check"></span> The action that you have done was a success! Well done', pos: 'top-right',status:'success',timeout:5000});
 </script>
 <?php require "template-parts/layouts/footer.php"; ?>
